c1d58c64f2afc8472fc7a5fbf9702ea8171cd778
spulakk <spulak.k@seznam.cz>
2018-05-04 10:46:38 +0200

    added topnav

--- temp/cache/Nette.RobotLoader/f739acb622498df3d9a7f6742567cdaf.php
+++ temp/cache/Nette.RobotLoader/f739acb622498df3d9a7f6742567cdaf.php
@@ -2,39 +2,39 @@
 return array (
   0 => 
   array (
-    'App\\Presenters\\Error4xxPresenter' => 
-    array (
-      'file' => 'C:\\xampp\\htdocs\\Nette_seminarka\\app\\presenters\\Error4xxPresenter.php',
-      'time' => 1524501802,
-    ),
-    'App\\Presenters\\ErrorPresenter' => 
-    array (
-      'file' => 'C:\\xampp\\htdocs\\Nette_seminarka\\app\\presenters\\ErrorPresenter.php',
-      'time' => 1524501802,
-    ),
-    'App\\Presenters\\ItemPresenter' => 
-    array (
-      'file' => 'C:\\xampp\\htdocs\\Nette_seminarka\\app\\presenters\\ItemPresenter.php',
-      'time' => 1525195767,
-    ),
     'App\\RouterFactory' => 
     array (
       'file' => 'C:\\xampp\\htdocs\\Nette_seminarka\\app\\router\\RouterFactory.php',
-      'time' => 1524501802,
+      'time' => 1525421457,
     ),
     'App\\Model\\GoodsManager' => 
     array (
       'file' => 'C:\\xampp\\htdocs\\Nette_seminarka\\app\\model\\GoodsManager.php',
-      'time' => 1525378693,
+      'time' => 1525422926,
+    ),
+    'App\\Presenters\\Error4xxPresenter' => 
+    array (
+      'file' => 'C:\\xampp\\htdocs\\Nette_seminarka\\app\\presenters\\Error4xxPresenter.php',
+      'time' => 1525421457,
+    ),
+    'App\\Presenters\\ErrorPresenter' => 
+    array (
+      'file' => 'C:\\xampp\\htdocs\\Nette_seminarka\\app\\presenters\\ErrorPresenter.php',
+      'time' => 1525421457,
+    ),
+    'App\\Presenters\\ItemPresenter' => 
+    array (
+      'file' => 'C:\\xampp\\htdocs\\Nette_seminarka\\app\\presenters\\ItemPresenter.php',
+      'time' => 1525421457,
     ),
     'App\\Presenters\\HomepagePresenter' => 
     array (
       'file' => 'C:\\xampp\\htdocs\\Nette_seminarka\\app\\presenters\\HomepagePresenter.php',
-      'time' => 1525382794,
+      'time' => 1525423448,
     ),
   ),
   1 => 
   array (
-    'Nette\\Environment' => 3,
+    'Nette\\Environment' => 4,
   ),
 );
--- temp/cache/latte/app-templates-@layout.latte--c7f6f8a0d6.php
+++ temp/cache/latte/app-templates-@layout.latte--c7f6f8a0d6.php
@@ -23,15 +23,14 @@ class Templatec7f6f8a0d6 extends Latte\Runtime\Template
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<link rel="stylesheet" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 7 */ ?>/css/style.css">
+	<link rel="stylesheet" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 6 */ ?>/css/style.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 9 */ ?>/bower_components/bootstrap/dist/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 10 */ ?>/bower_components/happy/dist/happy.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 11 */ ?>/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 12 */ ?>/bower_components/ublaboo-datagrid/assets/dist/datagrid.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 13 */ ?>/bower_components/ublaboo-datagrid/assets/dist/datagrid-spinners.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 14 */ ?>/bower_components/bootstrap-select/dist/css/bootstrap-select.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 8 */ ?>/bower_components/bootstrap/dist/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 9 */ ?>/bower_components/happy/dist/happy.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 10 */ ?>/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker3.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 11 */ ?>/bower_components/ublaboo-datagrid/assets/dist/datagrid.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 12 */ ?>/bower_components/ublaboo-datagrid/assets/dist/datagrid-spinners.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 13 */ ?>/bower_components/bootstrap-select/dist/css/bootstrap-select.css">
 
 	<title><?php
 		if (isset($this->blockQueue["title"])) {
@@ -49,11 +48,18 @@ class Templatec7f6f8a0d6 extends Latte\Runtime\Template
 		$iterations = 0;
 		foreach ($flashes as $flash) {
 			?>	<div<?php if ($_tmp = array_filter(['flash', $flash->type])) echo ' class="', LR\Filters::escapeHtmlAttr(implode(" ", array_unique($_tmp))), '"' ?>><?php
-			echo LR\Filters::escapeHtmlText($flash->message) /* line 20 */ ?></div>
+			echo LR\Filters::escapeHtmlText($flash->message) /* line 19 */ ?></div>
 <?php
 			$iterations++;
 		}
 ?>
+
+	<div class="topnav">
+		<a class="active" href="#home">Tabulka</a>
+		<a href="#news">-</a>
+		<a href="#contact">-</a>
+		<a href="#about">-</a>
+	</div>
 
 <?php
 		$this->renderBlock('content', $this->params, 'html');
@@ -73,7 +79,7 @@ class Templatec7f6f8a0d6 extends Latte\Runtime\Template
 	function prepare()
 	{
 		extract($this->params);
-		if (isset($this->params['flash'])) trigger_error('Variable $flash overwritten in foreach on line 20');
+		if (isset($this->params['flash'])) trigger_error('Variable $flash overwritten in foreach on line 19');
 		Nette\Bridges\ApplicationLatte\UIRuntime::initialize($this, $this->parentName, $this->blocks);
 		
 	}
@@ -82,17 +88,17 @@ class Templatec7f6f8a0d6 extends Latte\Runtime\Template
 	function blockScripts($_args)
 	{
 		extract($_args);
-		?>		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 25 */ ?>/bower_components/jquery/dist/jquery.js"></script>
-		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 26 */ ?>/bower_components/nette-forms/src/assets/netteForms.js"></script>
-		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 27 */ ?>/bower_components/nette.ajax.js/nette.ajax.js"></script>
-		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 28 */ ?>/bower_components/happy/dist/happy.js"></script>
-		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 29 */ ?>/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js"></script>
-		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 30 */ ?>/bower_components/jquery-ui-sortable/jquery-ui-sortable.js"></script>
-		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 31 */ ?>/bower_components/ublaboo-datagrid/assets/dist/datagrid.js"></script>
-		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 32 */ ?>/bower_components/ublaboo-datagrid/assets/dist/datagrid-instant-url-refresh.js"></script>
-		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 33 */ ?>/bower_components/ublaboo-datagrid/assets/dist/datagrid-spinners.js"></script>
-		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 34 */ ?>/bower_components/bootstrap/dist/js/bootstrap.js"></script>
-		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 35 */ ?>/bower_components/bootstrap-select/dist/js/bootstrap-select.js"></script>
+		?>		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 31 */ ?>/bower_components/jquery/dist/jquery.js"></script>
+		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 32 */ ?>/bower_components/nette-forms/src/assets/netteForms.js"></script>
+		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 33 */ ?>/bower_components/nette.ajax.js/nette.ajax.js"></script>
+		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 34 */ ?>/bower_components/happy/dist/happy.js"></script>
+		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 35 */ ?>/bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.js"></script>
+		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 36 */ ?>/bower_components/jquery-ui-sortable/jquery-ui-sortable.js"></script>
+		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 37 */ ?>/bower_components/ublaboo-datagrid/assets/dist/datagrid.js"></script>
+		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 38 */ ?>/bower_components/ublaboo-datagrid/assets/dist/datagrid-instant-url-refresh.js"></script>
+		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 39 */ ?>/bower_components/ublaboo-datagrid/assets/dist/datagrid-spinners.js"></script>
+		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 40 */ ?>/bower_components/bootstrap/dist/js/bootstrap.js"></script>
+		<script src="<?php echo LR\Filters::escapeHtmlAttr(LR\Filters::safeUrl($basePath)) /* line 41 */ ?>/bower_components/bootstrap-select/dist/js/bootstrap-select.js"></script>
 		<script>
             $.nette.init();
 		</script>
